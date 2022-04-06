0572363443749315cb76a7f86d07dba5c8abd1fa
johnmason <john.mason@ezyvet.com>
2022-04-06 16:38:40 +1200

    added react routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 //import logo from './logo.svg';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import LoginForm from '../components/LoginForm';
 import ClientList from '../Clients/Clients';
 
@@ -7,8 +8,16 @@ import ClientList from '../Clients/Clients';
 const App: React.FC = ()=>{
  
   return <div className="App">
-    {/* <LoginForm /> */}
-    <ClientList clients={[]}/>
+      <BrowserRouter>
+          <nav>
+              <Link to="clients">Clients</Link>
+          </nav>
+         <Routes>
+             <Route path="/login" element ={<LoginForm />} />
+             <Route path="/clients" element ={<ClientList clients={[]} />} />
+         </Routes>
+      </BrowserRouter>
+
   </div>;
 } 
 
